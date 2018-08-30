68281db850f9a50936878c12a5de117b7a22c0d1
luis zeni <luisfelipezeni@gmail.com>
2018-08-30 16:37:49 -0300

    more more

--- code/visualization/heatmap_from_detection.py
+++ code/visualization/heatmap_from_detection.py
@@ -1,15 +1,12 @@
-#import sys; sys.path.append('.')
-
 import cv2, os, time, argparse
 import numpy as np
 
 from keras import backend as K
-from pdb import set_trace as pause
 
 from yad2k.keras_yolo import yolo_eval, yolo_head, loadYoloModel
+
 from core.BoundBox import BoundBox
 from core.Utils import convertDetectionsToMyImp, loadAndNormalizeImg, image_resize
-
 from core.DisplayUtils import put_title
 
 
@@ -31,7 +28,6 @@ def heat_map(activation_output, img, yolo_model):
 
 	heatmap = cv2.resize(heatmap, (img[0].shape[1], img[0].shape[0]))
 	return heatmap
-
 
 def heatmap_title(title, activation_output, img, yolo_model):
 	print(activation_output)
@@ -59,7 +55,6 @@ def alpha_grad(grds, img):
 	for img_name  in grds:
 		grds[img_name] = grds[img_name] * 0.75 + img * 0.5
 
-
 def create_image_grid(grds, grid):
 	final = np.array([])
 	for group in grid:
@@ -77,174 +72,126 @@ def create_image_grid(grds, grid):
 			final = np.concatenate((final,line), axis=0)
 	return final
 
-
-
-
-
-
-
 def get_args():
  
 	parser = argparse.ArgumentParser(description='SIBGRAPI18 - Gender Activation Visualization\n')
 	parser.add_argument('-m', metavar='--model', type=str, required='True', help='tensorflow model in .h5 format\n')
 	parser.add_argument('-i', metavar='--image', type=str, help='input image\n')
 	parser.add_argument('-v', metavar='--video', type=str, help='input video\n')
+	parser.add_argument('-nw', metavar='--no_window', type=str, help='the code will not preset windoes, is recomended the usage of the -s option if you use -nw\n')
 	parser.add_argument('-s', metavar='--save',  type=str, help='location to save the result with file name \n')
 
-
 	args = parser.parse_args()
-
 	h5_model	= args.m
 	input_image = args.i
 	input_video = args.v
 	save_at	 = args.s
-
+	no_window = rgs.nw
 
 	video_mode = False
 	input_location = input_image
 	
+	if no_window is None: 
+		no_window = False
+	else:
+		no_window = True
+
+
 	if input_video is not None:
 		video_mode = True
 		input_location = input_video
 	elif input_image is None:
 		raise InputError('You need to inform a video or an image input') from error
 
+	return [h5_model, input_location, video_mode, save_at, no_window]
+
+def _main():		
+	[h5_model, input_location, video_mode, save_at, no_window] = get_args()
+
+	# print("Parameters: ", h5_model, input_location, video_mode, save_at)
+
+	colors = {'man': (0.9019, 0.7647, 0.6235), 
+			  'woman': (0.6470, 0.3568, 1.0)}
+
+	if not video_mode:
+		norm_grup_detections = ["man","woman"]
+		grid_detections = [["original","detection"], ["man","woman"]]
+				 
+
+		#load the tensorflow model, i am assuming that this code will be used to  gender detection cases, therefore, I hardcoded some data =p
+		yolo_model, anchors, class_names = loadYoloModel(h5_model, 'cfg/anchors.txt', 'cfg/gender_classes.txt')
 
 
-	return [h5_model, input_location, video_mode, save_at]
+		model_image_size = yolo_model.layers[0].input_shape[1:3]
+		img, img_size = loadAndNormalizeImg(input_location, model_image_size)
+
+		sess = K.get_session() 
+
+		yolo_outputs = yolo_head(yolo_model.output, anchors, len(class_names))
+		input_image_shape = K.placeholder(shape=(2, ))
+
+		boxes, scores, classes, boxes_scores = yolo_eval(yolo_outputs,
+														input_image_shape,
+														score_threshold=0.3,
+														iou_threshold=0.5,
+														grad_info=True)
 
 
-
-
-''' ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-~~~~~~~~~~~~~~~~~~~ MAIN STARTS HERE, i think ~~~~~~~~~~~~~~~~~~~~~
-   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ '''
-
-'''
-	Inputs ->
-		model
-		image/video
-		save at
-	
-'''		
+		out_boxes, out_scores, out_classes, out_boxes_scores = sess.run(
+													[boxes, scores, classes, boxes_scores],
+													feed_dict={
+													yolo_model.input: img,
+													input_image_shape: [img.shape[1], img.shape[2]],
+													K.learning_phase(): 0
+													})
 		
-[h5_model, input_location, video_mode, save_at] = get_args()
+		print('Found {} boxes'.format(len(out_boxes)))
 
-
-print("Parameters: ", h5_model, input_location, video_mode, save_at)
-
-colors = {'man': (0.9019, 0.7647, 0.6235), 
-		  'woman': (0.6470, 0.3568, 1.0)}
+		# convert detections to my representatio of 
+		detections = convertDetectionsToMyImp(out_boxes, out_scores, out_classes, class_names, img.shape)
 
 
 
+		# create image with all detected bounding boxes
+		imgAllDetections=img[0].copy()
+		for d in detections:
+			d.drawInImage(imgAllDetections, scale = 4, color=colors[d.classId], lineWidth=3, text=d.classId, alpha = 0.7)
 
 
-if not video_mode:
+		##TODO -> work with more than one detection
+		for i, d in enumerate(detections):
+			#finds were the detection was generated n the region tensor
+			l = np.where(out_boxes_scores == out_scores[i])
 
-	norm_grup_detections = ["man","woman"]
-	grid_detections = [["original","detection"], ["man","woman"]]
-			 
+			grds = {}
+			grds["man"]		= heat_map(yolo_outputs[3][l[0][0],l[1][0],l[2][0],l[3][0],0], img, yolo_model)
+			grds["woman"]	  = heat_map(yolo_outputs[3][l[0][0],l[1][0],l[2][0],l[3][0],1], img, yolo_model)
 
-	#load the tensorflow model, i am assuming that this code will be used to  gender detection cases, therefore, I hardcoded some data =p
-	yolo_model, anchors, class_names = loadYoloModel(h5_model, 'cfg/anchors.txt', 'cfg/gender_classes.txt')
+			normalize_grads(grds, norm_grup_detections)
 
+			detectionImg = img[0].copy()
 
-	model_image_size = yolo_model.layers[0].input_shape[1:3]
-	img, img_size = loadAndNormalizeImg(input_location, model_image_size)
+			d.drawInImage(detectionImg, scale = 4, color=colors[d.classId], lineWidth=3, text=d.classId, alpha = 0.7)
 
-	sess = K.get_session() 
+			alpha_grad(grds, detectionImg)
 
-	yolo_outputs = yolo_head(yolo_model.output, anchors, len(class_names))
-	input_image_shape = K.placeholder(shape=(2, ))
+			grds["detection"] = detectionImg
+			grds["original"] = img[0].copy()
+	
+			detectionGrad = create_image_grid(grds, grid_detections)
 
-	boxes, scores, classes, boxes_scores = yolo_eval(
-		yolo_outputs,
-		input_image_shape,
-		score_threshold=0.3,
-		iou_threshold=0.5,
-		grad_info=True)
-
-
-	out_boxes, out_scores, out_classes, out_boxes_scores = sess.run(
-		[boxes, scores, classes, boxes_scores],
-		feed_dict={
-		yolo_model.input: img,
-		input_image_shape: [img.shape[1], img.shape[2]],
-		K.learning_phase(): 0
-		})
-	print('Found {} boxes'.format(len(out_boxes)))
-
-
-
-	detections = convertDetectionsToMyImp(out_boxes, out_scores, out_classes, class_names, img.shape)
-
-
-	imgAllDetections=img[0].copy()
-
-	for d in detections:
-		d.drawInImage(imgAllDetections, scale = 4, color=colors[d.classId], lineWidth=3, text=d.classId, alpha = 0.7)
-
-
-	for i, d in enumerate(detections):
-
-		#finds were the detection was generated n the region tensor
-		l = np.where(out_boxes_scores == out_scores[i])
-
-
-		bath_idx = l[0][0]
-		x = l[1][0]
-		y = l[2][0]
-		anchor = l[3][0]
-		best_class = l[4][0]
-
-		loc = 7*anchor
-
-		imgs =[]
-		hts = []
-		superimposed_imgs = []
-
-
-		print(out_boxes_scores[l[0][0],l[1][0],l[2][0],l[3][0],0])
-		print(out_boxes_scores[l[0][0],l[1][0],l[2][0],l[3][0],1])
-
-		grds = {}
-		grds["man"]		= heat_map(yolo_outputs[3][l[0][0],l[1][0],l[2][0],l[3][0],0], img, yolo_model)
-		grds["woman"]	  = heat_map(yolo_outputs[3][l[0][0],l[1][0],l[2][0],l[3][0],1], img, yolo_model)
-
-
-		normalize_grads(grds, norm_grup_detections)
-
-		detectionImg = img[0].copy()
-
-		d.drawInImage(detectionImg, scale = 4, color=colors[d.classId], lineWidth=3, text=d.classId, alpha = 0.7)
-
-		alpha_grad(grds, detectionImg)
-
-		grds["detection"] = detectionImg
-		grds["original"] = img[0].copy()
-		print(classes.shape)
-		detectionGrad = create_image_grid(grds, grid_detections)
-
-		# detectionGrad = put_title("Normalization Groups: " + str(norm_grup_detections), detectionGrad, color = (0,0,0), background = (200,200,200), textScale=1.0,  widthScale = 2)
-
-	if True:
 		detectionGrad = image_resize(detectionGrad, height = img_size[0], width = img_size[1], inter = cv2.INTER_AREA)
-		# anchors	   = image_resize(anchors, height = 800, inter = cv2.INTER_AREA)
+		
+		if not no_window
+			cv2.imshow("SIBGRAPI18 - Gender Detection", detectionGrad)
+			key = cv2.waitKey(0)
 
-		cv2.imshow("This detectiosn Grads", detectionGrad)
-		# cv2.imshow("Anchors", anchors)
-		key = cv2.waitKey(0)
+		if save_at is not None:
+			cv2.imwrite(save_at, detectionGrad)
+			   
+		sess.close()
+		K.clear_session() 
 
-	else:
-
-		local = path_ + "outputs/" + enclosureFolder +  key + "/"
-		if not os.path.exists(local):
-			os.makedirs(local)
-
-		cv2.imwrite(local + imgName.replace(".jpg", "") + "_detec_" +  str(i) + ".jpg", detectionGrad*255)
-		   
-	sess.close()
-	K.clear_session() 
-
+if __name__ == '__main__':
+    _main()
 
