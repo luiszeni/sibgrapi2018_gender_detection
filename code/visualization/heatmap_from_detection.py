c5505b77229aa43fbe16c42e0ff2dcb1af18ec54
luis felipe zeni <luisfelipezeni@gmail.com>
2018-09-03 13:23:06 +0000

    I just wanted to eat at RU

--- code/visualization/heatmap_from_detection.py
+++ code/visualization/heatmap_from_detection.py
@@ -60,7 +60,7 @@ def create_image_grid(grds, grid):
 
 def detection_heatmap_proceess_frame(img, h5_model, max_detections=3, old_detections=None):
 	colors = {'man': (0.9019, 0.7647, 0.6235), 'woman': (0.6470, 0.3568, 1.0)}
-
+	print("max_detections", max_detections)
 	#load the tensorflow model, i am assuming that this code will be used to  gender detection cases, therefore, I hardcoded some data =p
 	yolo_model, anchors, class_names = loadYoloModel(h5_model, 'cfg/anchors.txt', 'cfg/gender_classes.txt')
 
@@ -92,10 +92,11 @@ def detection_heatmap_proceess_frame(img, h5_model, max_detections=3, old_detect
 
 	# convert detections to my representationnnn of bb 
 	detections = convert_detections_to_my_imp(out_boxes, out_scores, out_classes, class_names, img.shape)
+	detections = sorted(detections, key=lambda x: x.pred, reverse=True)
+	detections = detections[:max_detections]
 
 	images = []
 	for i, d in enumerate(detections):
-
 		#finds were the detection was generated n the region tensor
 		l = np.where(out_boxes_scores == out_scores[i])
 		grds = {}
@@ -140,13 +141,17 @@ def detection_heatmap_proceess_frame(img, h5_model, max_detections=3, old_detect
 
 
 def get_args():
- 
 	parser = argparse.ArgumentParser(description='SIBGRAPI18 - Gender Activation Visualization\n')
-	parser.add_argument('-m', metavar='--model', type=str, required='True', help='tensorflow model in .h5 format\n')
-	parser.add_argument('-i', metavar='--image', type=str, help='input image\n')
-	parser.add_argument('-v', metavar='--video', type=str, help='input video\n')
-	parser.add_argument('-nw', metavar='--no_window', type=str, help='the code will not preset windows, is recommended the usage of the -s option if you use -nw\n')
-	parser.add_argument('-s', metavar='--save',  type=str, help='location to save the result with file name \n')
+	parser.add_argument('-m',  metavar='--model',      type=str, required='True', help='tensorflow model in .h5 format\n')
+	parser.add_argument('-i',  metavar='--image',      type=str, help='input image\n')
+	parser.add_argument('-v',  metavar='--video',      type=str, help='input video\n')
+	parser.add_argument('-nw', metavar='--no_window',  type=bool, default=False, help='the code will not preset windows, is recommended the usage of the -s option if you use -nw\n')
+	parser.add_argument('-s',  metavar='--save',       type=str, help='full path to location to save the result with file name and file extension\n')
+
+	parser.add_argument('-sf', metavar='--starting_frame',  type=int, default=0,  help='Video Mode: Frame from whith the application  starts processing\n')
+	parser.add_argument('-mf', metavar='--max_frame',       type=int, default=-1, help='Video Mode: Amount of frames  whith the application processes agter the starting frame\n')
+	parser.add_argument('-skf',metavar='--skip_frames',     type=int, default=1,  help='Video Mode: Number of frames to skip during processing (default is one)\n')
+	parser.add_argument('-md', metavar='--max_detections',  type=int, default=2,  help='Video Mode: Number of detections which will be displayed (default is two)\n')
 
 	args = parser.parse_args()
 	h5_model	= args.m
@@ -155,14 +160,13 @@ def get_args():
 	save_at	 = args.s
 	no_window = args.nw
 
+	starting_frame = args.sf
+	max_frame = args.mf
+	skip_frames = args.skf
+	max_detections = args.md
+
 	video_mode = False
 	input_location = input_image
-	
-	if no_window is None: 
-		no_window = False
-	else:
-		no_window = True
-
 
 	if input_video is not None:
 		video_mode = True
@@ -170,69 +174,75 @@ def get_args():
 	elif input_image is None:
 		raise InputError('You need to inform a video or an image input') from error
 
-	return [h5_model, input_location, video_mode, save_at, no_window]
+	return {"h5_model":h5_model, "input_location":input_location, "video_mode":video_mode, 
+			"save_at":save_at, "no_window":no_window, "starting_frame":starting_frame, 
+			"max_frame":max_frame, "skip_frames":skip_frames, "max_detections":max_detections}
 
 def _main():		
-	[h5_model, input_location, video_mode, save_at, no_window] = get_args()
+	args = get_args()
+	if not args["video_mode"]:
+		img = cv2.imread(args["input_location"])
+		# output_img = detection_heatmap_proceess_frame(img, args["h5_model"])
+		output_img, old_detections =  detection_heatmap_proceess_frame(img, args["h5_model"], args["max_detections"], [])
 
-	# print("Parameters: ", h5_model, input_location, video_mode, save_at)
-	if not video_mode:
-		img = cv2.imread(input_location)
-		output_img = detection_heatmap_proceess_frame(img, h5_model)
+		if not args["no_window"]:
+			to_show = image_resize(output_img, height = 800, inter = cv2.INTER_AREA)
+			cv2.imshow("SIBGRAPI18 - Gender Detection", to_show)
+			cv2.waitKey(0)
 
-		if not no_window:
-			cv2.imshow("SIBGRAPI18 - Gender Detection", output_img)
-			key = cv2.waitKey(0)
+		if args["save_at"] is not None:
+			output_img *= 255
+			output_img = output_img.astype(np.uint8)
+			cv2.imwrite(args["save_at"] , output_img)
 
-		if save_at is not None:
-			cv2.imwrite(save_at, output_img)
 	else:
-	
-		starting_frame = 25
-		max_frame = 650
-		skip_frames = 1
-		max_detections = 2
-
-
-		print("video", input_location)
-		cap = cv2.VideoCapture(input_location)
-
+		cap = cv2.VideoCapture(args["input_location"])
+		print("args[max_detections]", args["max_detections"])
 		frame_size = None
 		if not cap.isOpened(): 
-		   print("uops, not able to pen the capture on opencv... =(")
+		   print("uops, not able to open the capture in opencv... =(")
 		else:
 			frame_size = int(cap.get(3)), int(cap.get(4))
-			for f in range(0, starting_frame):
+			for f in range(0, args["starting_frame"]):
 				ret, frame = cap.read()
-				cv2.imshow('frame',frame)
-				#out.write(output_img)
-				if cv2.waitKey(1) & 0xFF == ord('q'):
+				if ret is not True:
 					break
+				if not args["no_window"]:
+					cv2.imshow('SIBGRAPI18 - Gender Detection',frame)
+					if cv2.waitKey(1) == 27:
+						break
 
 			old_detections = None
-			fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-			out = cv2.VideoWriter('output.avi',fourcc, 24.0, (frame_size[0]*max_detections,frame_size[1]*2))
-
-			for f in range(0, max_frame):
-				print("Frame ", f)
+			if args["save_at"] is not None:
+				fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+				out = cv2.VideoWriter(args["save_at"],fourcc, 24.0, (frame_size[0]*args["max_detections"],frame_size[1]*2))
+			
+			f = 0
+			while f < args["max_frame"] or args["max_frame"] is -1:
 				ret, frame = cap.read()
-				if f % skip_frames is 0:
+				if ret is not True:
+					break
+				if f % args["skip_frames"] is 0:
 					# Beer is life, don't you think?
-					output_img, old_detections =  detection_heatmap_proceess_frame(frame, h5_model, max_detections, old_detections)
+					print("Processing Frame:", args["starting_frame"] + f)
+
+					output_img, old_detections =  detection_heatmap_proceess_frame(frame, args["h5_model"], args["max_detections"], old_detections)
 
 					output_img *= 255
 					output_img = output_img.astype(np.uint8)	
-					out.write(output_img)
+					if args["save_at"] is not None:
+						out.write(output_img)
 
-					if not no_window:
+					if not args["no_window"]:
 						to_show = image_resize(output_img, height = 800, inter = cv2.INTER_AREA)
-						cv2.imshow('frame',to_show)
-						if cv2.waitKey(10) & 0xFF == ord('q'):
+						cv2.imshow('SIBGRAPI18 - Gender Detection',to_show)
+						if cv2.waitKey(1) == 27:
 							break
-
+				f+=1
 			cap.release()
 			cv2.destroyAllWindows()
-			out.release()
+			if args["save_at"] is not None:
+				out.release()
 
 if __name__ == '__main__':
 	_main()
